4203a226446549787e345b7817bb48f6712421f7
Bobby <bobbybabu@gmail.com>
2023-11-01 20:30:06 -0400

    mimicing Swathis commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Learning DevOps with Git from Bobby"
+    return "Learning DevOps with Git from Swathi"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
